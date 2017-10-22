defc12432cab40e83028e390dcaf61efa7108b71
Moore <mooreb@VIMNY-MOOREB-LM.vimeo.iac.corp>
2017-10-22 18:15:52 -0400

    Some styling things

--- resources/public/js/compiled/out/snail/views.cljs
+++ resources/public/js/compiled/out/snail/views.cljs
@@ -3,7 +3,7 @@
             [cljs.pprint :refer [pprint]]))
 
 (def number-steez
-    {:display "inline-block" :font-family "Courier New" :border "1px solid white" :width "3rem"})
+    {:display "inline-block" :text-align "center" :line-height 1.5 :font-family "Courier New" :border "1px solid white" :width "3rem"})
 
 (defn number-item [color n]
   [:span
@@ -14,11 +14,12 @@
   (let [items (re-frame/subscribe [:items])
         unraveled-items (re-frame/subscribe [:unravel-items])]
     [:div
+      {:style {:padding "20px" :border-top "2px solid salmon"}}
       ; Show log of the db at the top of the page:
       ; [:pre
       ;   {:style {:background-color "cornsilk" :color "cornflowerblue" :padding "10px"}}
       ;   (with-out-str (pprint @re-frame.db/app-db))]
-      [:h2 "Behold! The rotating matrix!"]
+      [:h2 "Behold! The unraveling snail!"]
       [:input
         {:type "number"
          :default-value 5
@@ -26,18 +27,17 @@
       [:button
          {:on-click #(re-frame/dispatch
                        [:create-matrix
-                        (.parseInt js/window (-> (.querySelector js/document "#row-count") .-value))])} "Create the matrix!"]
+                        (.parseInt js/window (-> (.querySelector js/document "#row-count") .-value))])} "Create the snail!"]
       ; [:button
       ;    {:on-click #(re-frame/dispatch [:rotate])}
       ;    "Rotate the matrix!"]
       ; [:button
       ;    {:on-click #(re-frame/dispatch [:unravel-matrix])} "Unravel the matrix!"]
       [:button
-         {:on-click #(re-frame/dispatch [:tick-unravel])} "Start the unraveling!"]
+         {:on-click #(re-frame/dispatch [:tick-unravel])} "Unravel the snail!"]
       [:div
-        [:h2 "The unraveled items"]
         [:p (map (partial number-item "cornsilk") @unraveled-items)]]
       (for [row @items]
         [:div {:style {:font-family "Courier"}}
-          [:h2
+          [:div {:style {:font-size "24px" :font-weight "bold"}}
             (map (partial number-item "cornflowerblue") row)]])]))
--- resources/public/js/compiled/out/snail/views.js
+++ resources/public/js/compiled/out/snail/views.js
@@ -3,42 +3,42 @@ goog.provide('snail.views');
 goog.require('cljs.core');
 goog.require('re_frame.core');
 goog.require('cljs.pprint');
-snail.views.number_steez = new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"display","display",242065432),"inline-block",new cljs.core.Keyword(null,"font-family","font-family",-667419874),"Courier New",new cljs.core.Keyword(null,"border","border",1444987323),"1px solid white",new cljs.core.Keyword(null,"width","width",-384071477),"3rem"], null);
+snail.views.number_steez = new cljs.core.PersistentArrayMap(null, 6, [new cljs.core.Keyword(null,"display","display",242065432),"inline-block",new cljs.core.Keyword(null,"text-align","text-align",1786091845),"center",new cljs.core.Keyword(null,"line-height","line-height",1870784992),1.5,new cljs.core.Keyword(null,"font-family","font-family",-667419874),"Courier New",new cljs.core.Keyword(null,"border","border",1444987323),"1px solid white",new cljs.core.Keyword(null,"width","width",-384071477),"3rem"], null);
 snail.views.number_item = (function snail$views$number_item(color,n){
 return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"span","span",1394872991),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),cljs.core.assoc.call(null,snail.views.number_steez,new cljs.core.Keyword(null,"background-color","background-color",570434026),color)], null),[" ",cljs.core.str.cljs$core$IFn$_invoke$arity$1(n)].join('')], null);
 });
 snail.views.main_panel = (function snail$views$main_panel(){
 var items = re_frame.core.subscribe.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"items","items",1031954938)], null));
 var unraveled_items = re_frame.core.subscribe.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"unravel-items","unravel-items",-461717224)], null));
-return new cljs.core.PersistentVector(null, 7, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2","h2",-372662728),"Behold! The rotating matrix!"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"number",new cljs.core.Keyword(null,"default-value","default-value",232220170),(5),new cljs.core.Keyword(null,"id","id",-1388402092),"row-count"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),((function (items,unraveled_items){
+return new cljs.core.PersistentVector(null, 8, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"padding","padding",1660304693),"20px",new cljs.core.Keyword(null,"border-top","border-top",-158897573),"2px solid salmon"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2","h2",-372662728),"Behold! The unraveling snail!"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"number",new cljs.core.Keyword(null,"default-value","default-value",232220170),(5),new cljs.core.Keyword(null,"id","id",-1388402092),"row-count"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),((function (items,unraveled_items){
 return (function (){
 return re_frame.core.dispatch.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"create-matrix","create-matrix",-267625253),window.parseInt(document.querySelector("#row-count").value)], null));
 });})(items,unraveled_items))
-], null),"Create the matrix!"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),((function (items,unraveled_items){
+], null),"Create the snail!"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),((function (items,unraveled_items){
 return (function (){
 return re_frame.core.dispatch.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"tick-unravel","tick-unravel",-482315631)], null));
 });})(items,unraveled_items))
-], null),"Start the unraveling!"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2","h2",-372662728),"The unraveled items"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),cljs.core.map.call(null,cljs.core.partial.call(null,snail.views.number_item,"cornsilk"),cljs.core.deref.call(null,unraveled_items))], null)], null),(function (){var iter__29094__auto__ = ((function (items,unraveled_items){
-return (function snail$views$main_panel_$_iter__42134(s__42135){
+], null),"Unravel the snail!"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),cljs.core.map.call(null,cljs.core.partial.call(null,snail.views.number_item,"cornsilk"),cljs.core.deref.call(null,unraveled_items))], null)], null),(function (){var iter__29094__auto__ = ((function (items,unraveled_items){
+return (function snail$views$main_panel_$_iter__31776(s__31777){
 return (new cljs.core.LazySeq(null,((function (items,unraveled_items){
 return (function (){
-var s__42135__$1 = s__42135;
+var s__31777__$1 = s__31777;
 while(true){
-var temp__4657__auto__ = cljs.core.seq.call(null,s__42135__$1);
+var temp__4657__auto__ = cljs.core.seq.call(null,s__31777__$1);
 if(temp__4657__auto__){
-var s__42135__$2 = temp__4657__auto__;
-if(cljs.core.chunked_seq_QMARK_.call(null,s__42135__$2)){
-var c__29092__auto__ = cljs.core.chunk_first.call(null,s__42135__$2);
+var s__31777__$2 = temp__4657__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__31777__$2)){
+var c__29092__auto__ = cljs.core.chunk_first.call(null,s__31777__$2);
 var size__29093__auto__ = cljs.core.count.call(null,c__29092__auto__);
-var b__42137 = cljs.core.chunk_buffer.call(null,size__29093__auto__);
-if((function (){var i__42136 = (0);
+var b__31779 = cljs.core.chunk_buffer.call(null,size__29093__auto__);
+if((function (){var i__31778 = (0);
 while(true){
-if((i__42136 < size__29093__auto__)){
-var row = cljs.core._nth.call(null,c__29092__auto__,i__42136);
-cljs.core.chunk_append.call(null,b__42137,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"font-family","font-family",-667419874),"Courier"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2","h2",-372662728),cljs.core.map.call(null,cljs.core.partial.call(null,snail.views.number_item,"cornflowerblue"),row)], null)], null));
+if((i__31778 < size__29093__auto__)){
+var row = cljs.core._nth.call(null,c__29092__auto__,i__31778);
+cljs.core.chunk_append.call(null,b__31779,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"font-family","font-family",-667419874),"Courier"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"font-size","font-size",-1847940346),"24px",new cljs.core.Keyword(null,"font-weight","font-weight",2085804583),"bold"], null)], null),cljs.core.map.call(null,cljs.core.partial.call(null,snail.views.number_item,"cornflowerblue"),row)], null)], null));
 
-var G__42138 = (i__42136 + (1));
-i__42136 = G__42138;
+var G__31780 = (i__31778 + (1));
+i__31778 = G__31780;
 continue;
 } else {
 return true;
@@ -46,13 +46,13 @@ return true;
 break;
 }
 })()){
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__42137),snail$views$main_panel_$_iter__42134.call(null,cljs.core.chunk_rest.call(null,s__42135__$2)));
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__31779),snail$views$main_panel_$_iter__31776.call(null,cljs.core.chunk_rest.call(null,s__31777__$2)));
 } else {
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__42137),null);
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__31779),null);
 }
 } else {
-var row = cljs.core.first.call(null,s__42135__$2);
-return cljs.core.cons.call(null,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"font-family","font-family",-667419874),"Courier"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2","h2",-372662728),cljs.core.map.call(null,cljs.core.partial.call(null,snail.views.number_item,"cornflowerblue"),row)], null)], null),snail$views$main_panel_$_iter__42134.call(null,cljs.core.rest.call(null,s__42135__$2)));
+var row = cljs.core.first.call(null,s__31777__$2);
+return cljs.core.cons.call(null,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"font-family","font-family",-667419874),"Courier"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"font-size","font-size",-1847940346),"24px",new cljs.core.Keyword(null,"font-weight","font-weight",2085804583),"bold"], null)], null),cljs.core.map.call(null,cljs.core.partial.call(null,snail.views.number_item,"cornflowerblue"),row)], null)], null),snail$views$main_panel_$_iter__31776.call(null,cljs.core.rest.call(null,s__31777__$2)));
 }
 } else {
 return null;
@@ -67,4 +67,4 @@ return iter__29094__auto__.call(null,cljs.core.deref.call(null,items));
 })()], null);
 });
 
-//# sourceMappingURL=views.js.map?rel=1508709491149
+//# sourceMappingURL=views.js.map?rel=1508710473670
